04546a1a573a86bcd1e49cbd879e0cb93b4a198b
BZhang <zhang63@illinois.edu>
2012-02-23 18:50:35 -0600

    added tests for remove+nonexistant user

--- users/tests.py
+++ users/tests.py
@@ -6,11 +6,36 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
+from django.utils import simplejson
+
+from piston.utils import rc
+
+import base64
+from StringIO import StringIO
+
+from models import *
+from django.db import models
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class RemoveAccountTest(TestCase):
+
+    username = 'lolwut'
+    password = 'lolhai'
+    email = 'lol@wut.com'
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(self.username,self.username,self.password)
+        
+
+    def testRemoveUser (self):
+        response = self.client.post('/users/remove/',{'username': self.username, 'password': self.password})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, "Succesful account removal")
+
+    def testRemoveNonexistant(self):
+        response = self.client.post('/users/remove/',{'username': 'banana', 'password': 'lol'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, "Your username or password was invalid")
