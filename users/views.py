f8eac1dabeebbd21dd4f26a27f617a7edc983ba1
Alex Sikora <zaxxle@gmail.com>
2012-02-26 15:50:15 -0600

    More user tests

--- users/views.py
+++ users/views.py
@@ -5,28 +5,31 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from models import UserProfile
 from django.db import IntegrityError
+from django.views.decorators.csrf import csrf_protect
 
 def create_user(request):
     username = request.POST['username']
     password = request.POST['password']
     try:
         newUser = User.objects.create_user(username, username, password)
-        return HttpResponse("Account succesfully created");
+        return HttpResponse("Account successfully created");
     except IntegrityError:
         return HttpResponse("Account creation failed");
 
+@csrf_protect
 def authenticate_user(request):
     name = request.POST['username']
     passw = request.POST['password']
     user = authenticate(username=name, password=passw)
     if user is not None:
         if user.is_active:
-            return HttpResponse("Succesful authentication")
+            return HttpResponse("Successful authentication")
         else:
             return HttpResponse("Your account has been disabled")
     else:
         return HttpResponse("Your username or password was invalid")
 
+@csrf_protect
 def remove_user(request):
     username = request.POST['username']
     password = request.POST['password']
@@ -34,7 +37,7 @@ def remove_user(request):
     if user is not None:
         if user.is_active:
             user.delete()
-            return HttpResponse("Succesful account removal")
+            return HttpResponse("Successful account removal")
         else:
             return HttpResponse("Your account has been disabled")
     else:
