ab567a7dbb8a905a90bd3b49586a1378ab6988ca
Andrew <akilins2@illinois.edu>
2012-03-29 19:48:05 -0500

    server side add/remove favorite player/team/event completed.

--- users/models.py
+++ users/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from players.models import Player, Team
+from events.models import Event
 from django.db.models.signals import post_save
 # Create your models here.
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    favorite_players = models.ManyToManyField(Player)
+    favorite_teams = models.ManyToManyField(Team)
+    favorite_events = models.ManyToManyField(Event)
     
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
